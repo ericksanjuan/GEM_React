76b734db965aef7cbf1bc21e75c853bad3faa639
Athalfmile <greg.kelley.hk61@statefarm.com>
2023-03-17 13:07:03 -0400

    Navbar component displaying on page

---
+++ gem-application/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div class="navbar">
+      <div class="home">GEM Gardening - Plant Hardiness Zone Locator</div>
+      <a
+        href="https://en.wikipedia.org/wiki/Hardiness_zone"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        What is Hardiness
+      </a>
+    </div>
+  );
+}
+
+export default Navbar;
