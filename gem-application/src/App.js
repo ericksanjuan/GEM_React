feaa30e60301043ea87fd597b5df8ef1dc23cbab
Athalfmile <greg.kelley.hk61@statefarm.com>
2023-03-17 14:03:22 -0400

    added form

--- gem-application/src/App.js
+++ gem-application/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Form from './components/form';
+ 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+    <img alt="zones" src="./Images/USDA_zones.png"/>  
+    <div className="form">
+      <Form />
+    </div>
     </div>
   );
 }
